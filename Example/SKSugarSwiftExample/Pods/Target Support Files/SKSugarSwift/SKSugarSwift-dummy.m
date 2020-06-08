502fcacf2c496d8a37b37f99f40c5d21ff09ae5a
youlong <tonycgao@sina.com>
2020-06-08 16:16:12 +0800

    pod

---
+++ Example/SKSugarSwiftExample/Pods/Target Support Files/SKSugarSwift/SKSugarSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SKSugarSwift : NSObject
+@end
+@implementation PodsDummy_SKSugarSwift
+@end
--- Example/SKSugarSwiftExample/SKSugarSwiftExample/ViewController.swift
+++ Example/SKSugarSwiftExample/SKSugarSwiftExample/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var label = UILabel("哈哈哈", fontSize: 20, color: UIColor.purple, alignment: .right, isBold: true)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        view.addSubview(label)
+        label.backgroundColor = UIColor.orange
+        
     }
 
 
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        label.frame = CGRect(x: 20, y: 100, width: 200, height: 50)
+    }
 }
 
